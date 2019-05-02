2c759c1ab2c1b19a4e84dcee79c33eb078140edf
DESKTOP-R3DH7RO\evandro.araujo <evandro.araujo@linnks.com.br>
2019-05-02 08:15:43 -0300

    adicionando o model cidade

---
+++ src/app/model/cidade.model.ts
@@ -0,0 +1,19 @@
+
+export class Cidade {
+  id: number;
+  cidade : string;
+  uf : string;
+
+  constructor(cidade?: Cidade | number) {
+    if (cidade !== undefined && cidade !== null) {
+      if (typeof cidade === 'number') {
+        this.id = cidade;
+      } else {
+        this.id = cidade.id;
+        this.cidade = cidade.cidade;
+        this.uf =  cidade.uf;
+      }
+    }
+  }
+
+}
